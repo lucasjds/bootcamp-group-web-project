11b94f7cc4fecf3ec4a65cd85210c7289627fdd3
Lucas Souza <lucassouza690@gmail.com>
2018-06-22 00:22:02 -0400

    created moves class

---
+++ js/script.js
@@ -0,0 +1,197 @@
+
+class Pokemon{
+  constructor(id,name,image,types,height, weight, stats){
+  	this._id = id;
+    this._name = name;
+    this._image = image;
+    this._types = types;
+    this._height = height;
+    this._weight = weight;
+    this._stats = stats;
+    this._moves = [];
+  }
+  
+  getId(){
+  	return this._id;
+  }
+  
+ 	getName(){
+  	return this._name
+  }
+  
+  getImage(){
+  	return this._image;
+  }
+  
+  getTypes(){
+  	return this._types;
+  }
+  
+  getHeight(){
+  	return this._height;
+  }
+  
+  getWeight(){
+  	return this._weight;
+  }
+  
+  getStats(){
+  	return this._stats;
+  }
+  
+  getMoves(){
+  	return this._moves;
+  }
+  
+
+  toString(){
+		return this._name + "! A pokemon of " + this._types.map(a => a.getName()) + " type!";
+  }
+}
+
+class Type{
+	constructor(){
+  	
+  }
+  
+  setName(name){
+  	this._name = name;
+  }
+  
+  getName(){
+  	return this._name;
+  }
+}
+
+class Stats{
+	constructor(speed,specialDefense,specialAttack, defense, attack , hp){
+  	
+  	this._speed = speed;
+    this._specialDefense = specialDefense;
+    this._specialAttack = specialAttack;
+    this._defense = defense;
+    this._attack = attack;
+    this._hp = hp;
+    this._accuracy = 0;
+    this._evasion = 0;
+  }
+  
+  getEvasion(){
+  	return this._evasion;
+  }
+  
+  getAccuracy(){
+  	return this._accuracy;
+  }
+  
+  getSpeed(){
+  	return this._speed;
+  }
+  
+  getSpecialDefense(){
+  	return this._specialDefense;
+  }
+  
+  getSpecialAttack(){
+  	return this._specialAttack;
+  }
+  
+  getDefense(){
+  	return this._defense;
+  }
+  
+  getAttack(){
+  	return this._attack;
+  }
+  
+  getHP(){
+  	return this._hp;
+  }
+}
+
+class Move{
+	constructor(name){
+  	this._name = name;
+  }
+  
+  getName(name){
+  	return this._name;
+  }
+
+}
+
+//const $pokemons = $("#pokemons");
+const addPokemon = (pokemon) => {
+	document.getElementById("idpokemon").innerHTML = pokemon.getId();
+  document.getElementById("pokename").innerHTML = pokemon.getName();
+  document.getElementById("pokeimage").src = pokemon.getImage();
+  document.getElementById("type").innerHTML = pokemon.getTypes().map(a => a.getName());
+  document.getElementById("height").innerHTML = pokemon.getHeight();
+  document.getElementById("weight").innerHTML = pokemon.getWeight();
+  document.getElementById("hp").innerHTML = pokemon.getStats().getHP();
+  document.getElementById("attack").innerHTML = pokemon.getStats().getAttack();
+  document.getElementById("defense").innerHTML = pokemon.getStats().getDefense();
+  document.getElementById("spattack").innerHTML = pokemon.getStats().getSpecialAttack();
+  document.getElementById("spdefense").innerHTML = pokemon.getStats().getSpecialDefense();
+  document.getElementById("speed").innerHTML = pokemon.getStats().getSpeed();
+  
+  //responsiveVoice.speak(pokemon.toString());
+};
+
+const searchPokemon = (event) => {
+		event.preventDefault();
+    var param = document.getElementById("pokenumber").value;
+    var pokeURL = "https://pokeapi.co/api/v2/pokemon/" + param;
+    
+    $.ajax({
+      url: pokeURL,
+      dataType:'json',
+    }).then(data => {
+    	//console.log(data);
+      const types = createArrayType(data.types);
+     
+      const stats = new Stats(data.stats[0].base_stat,
+      												data.stats[1].base_stat,
+                              data.stats[2].base_stat,
+                              data.stats[3].base_stat,
+                              data.stats[4].base_stat,
+                              data.stats[5].base_stat);
+   		searchMove(data.moves[0].move.url);
+      const pokemon = new Pokemon(
+      										"#" + data.id,
+      										data.name.toUpperCase(), 
+                          data.sprites.front_default ,
+                          types,
+                          data.height,
+                          data.weight,
+                          stats);
+   	
+      addPokemon(pokemon);
+    }).catch( e  => console.log(e));
+}
+
+const searchMove = (url) => {
+		
+    $.ajax({
+      url: url,
+      dataType:'json',
+    }).then(data => {
+    	console.log(data);
+		const move = new Move(data.name);
+      
+    }).catch( e  => console.log(e));
+}
+
+function createArrayType(data){
+	var types = [];
+  var type = new Type();
+	data.forEach(function(element) {
+    type.setName(element.type.name);
+    types.push(type);
+    type = new Type();
+  });
+  return types;
+}
+window.onload=function(){
+	document.getElementById("pokeform").addEventListener('submit', searchPokemon);
+};
